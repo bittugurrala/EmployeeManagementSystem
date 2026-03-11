c5a68fd9c30bdab5540fb3b29040b437be7395a8
Bittu Gurrala <bittugurrala241@gmail.com>
2026-03-11 15:51:26 +0530

    Intial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a React based Employee management system, using Local storage for Data storage
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-3xl'>App</div>
+  )
+}
+
+export default App
