a80b945d1e59d115368283129cfe3ea36e3509fe
Bittu Gurrala <bittugurrala241@gmail.com>
2026-03-14 19:15:27 +0530

    fixed the labels and inputs

--- src/others/CreateTask.jsx
+++ src/others/CreateTask.jsx
@@ -5,26 +5,26 @@ export default function CreateTaks(){
                         <form className="w-full flex gap-5">
                             <div className="w-1/2">
                                 <div className = "flex flex-col gap-1 mb-5">
-                                    <label>Task Title</label>
-                                    <input className="border-white border  rounded-2xl text-gray-300 px-2 py-3 outline-none" type= "text" placeholder="Make UI design"></input>
+                                    <label htmlFor="title">Task Title</label>
+                                    <input className="border-white border  rounded-2xl text-gray-300 px-2 py-3 outline-none" id = "title" type= "text" placeholder="Make UI design"></input>
                                 </div>
                                 <div className = "flex flex-col gap-2 mb-5">
-                                    <label>Date</label>
-                                    <input className="border-white border rounded-2xl text-gray-300 px-2 py-3 outline-none" type="date" placeholder="dd/mm/yy"></input>
+                                    <label htmlFor="date">Date</label>
+                                    <input id = "date" className="border-white border rounded-2xl text-gray-300 px-2 py-3 outline-none" type="date" placeholder="dd/mm/yy"></input>
                                 </div>
                                 <div className = "flex flex-col gap-2 mb-5">
-                                    <label>Assign to</label>
-                                    <input className="border-white border rounded-2xl text-gray-300 px-2 py-3 outline-none" type= "text" placeholder="Employee name"></input>
+                                    <label htmlFor="assignTo">Assign to</label>
+                                    <input id = "assignTo" className="border-white border rounded-2xl text-gray-300 px-2 py-3 outline-none" type= "text" placeholder="Employee name"></input>
                                 </div>
                                 <div className = "flex flex-col gap-2 mb-5">
-                                    <label>Category</label>
-                                    <input className="border-white border rounded-2xl text-gray-300 px-2 py-3 outline-none" type="text" placeholder="Design, dev, etc"></input>
+                                    <label htmlFor="cat">Category</label>
+                                    <input id="cat" className="border-white border rounded-2xl text-gray-300 px-2 py-3 outline-none" type="text" placeholder="Design, dev, etc"></input>
                                 </div>
                             </div>
                             <div className="w-1/2">
                                 <div className = "flex flex-col  gap-1 mb-5">
-                                    <label>Description</label>
-                                    <textarea className="border-white border h-71.25 text-gray-300 rounded-2xl px-2 py-3 " placeholder="Description of the Task"></textarea>
+                                    <label htmlFor="desc">Description</label>
+                                    <textarea id="desc" className="border-white border h-71.25 text-gray-300 rounded-2xl px-2 py-3 " placeholder="Description of the Task"></textarea>
                                 </div>
                                 <button className="bg-amber-400 px-2 py-3 rounded-2xl w-full active:scale-90">Create Task</button>
                             </div>
